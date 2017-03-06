b7a09840356052bdce056e57a1ee0d38ec52cb18
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-03-06 15:27:22 -0500

    adding contacts ot the superhero seed file

---
+++ app.js
@@ -0,0 +1,12 @@
+// superheros and their information
+let superheroData = [
+  {name: 'Gambit', phone number: '111-111-1111', comic affiliation: 'Marvel', img_url: 'https://i.annihil.us/u/prod/marvel/i/mg/2/30/537baaede2ff3.jpg'}
+  {name: 'Gambit', phone number: '222-222-2222', comic affiliation: 'Marvel', img_url: 'https://i.annihil.us/u/prod/marvel/i/mg/2/30/537baaede2ff3.jpg'}
+  {name: 'Gambit', phone number: '333-333-3333', comic affiliation: 'Marvel', img_url: 'https://i.annihil.us/u/prod/marvel/i/mg/2/30/537baaede2ff3.jpg'}
+  {name: 'Gambit', phone number: '444-444-4444', comic affiliation: 'Marvel', img_url: 'https://i.annihil.us/u/prod/marvel/i/mg/2/30/537baaede2ff3.jpg'}
+  {name: 'Gambit', phone number: '555-555-5555', comic affiliation: 'Marvel', img_url: 'https://i.annihil.us/u/prod/marvel/i/mg/2/30/537baaede2ff3.jpg'}
+  {name: 'Gambit', phone number: '777-777-7777', comic affiliation: 'Marvel', img_url: 'https://i.annihil.us/u/prod/marvel/i/mg/2/30/537baaede2ff3.jpg'}
+  {name: 'Gambit', phone number: '888-888-8888', comic affiliation: 'Marvel', img_url: 'https://i.annihil.us/u/prod/marvel/i/mg/2/30/537baaede2ff3.jpg'}
+  {name: 'Beast', phone number: '999-999-9999', comic affiliation: 'Marvel', img_url: 'https://i.annihil.us/u/prod/marvel/i/mg/2/30/537baaede2ff3.jpg'}
+
+]
